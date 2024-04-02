6a7434219249e89be1f7149c60f58c8a9a9bf044
Anthony Marrello <apm817@gmail.com>
2024-04-02 18:46:42 -0400

    change money formatting sign

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,14 +5,12 @@ import { moneyFormatter } from '../utils/format';
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
 
-  const sign = transaction.amount < 0 ? '-' : '+';
+  //const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
-      <span>
-        {sign} {moneyFormatter(transaction.amount)}
-      </span>
+      <span>{moneyFormatter(transaction.amount)}</span>
       <button
         onClick={() => deleteTransaction(transaction.id)}
         className="delete-btn"
